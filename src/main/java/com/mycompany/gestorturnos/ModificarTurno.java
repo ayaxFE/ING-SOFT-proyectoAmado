796b397ebfb4860ba027f18416a8595a90ba3653
FacuDeRenzo <127690457+FacuDeRenzo@users.noreply.github.com>
2025-11-02 00:51:43 -0300

    Mas modificacion Modificar

--- src/main/java/com/mycompany/gestorturnos/ModificarTurno.java
+++ src/main/java/com/mycompany/gestorturnos/ModificarTurno.java
@@ -23,28 +23,125 @@ public class ModificarTurno extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        TextoInicial = new javax.swing.JLabel();
+        Texto1 = new javax.swing.JLabel();
+        Texto2 = new javax.swing.JLabel();
+        TextoMotivo = new javax.swing.JLabel();
+        ComboSelector = new javax.swing.JComboBox<>();
+        TextoDia = new javax.swing.JTextField();
+        TextoConsulta = new javax.swing.JTextField();
+        BotonAceptar = new javax.swing.JButton();
+        BotonCancelar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
-        jLabel1.setText("Modificar datos del paciente");
+        TextoInicial.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        TextoInicial.setText("Modificar datos del paciente");
+
+        Texto1.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        Texto1.setText("Ingresar el nuevo horario del turno");
+
+        Texto2.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        Texto2.setText("Ingresar el nuevo dia del turno");
+
+        TextoMotivo.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        TextoMotivo.setText("Ingresar el nuevo motivo de la consulta del turno");
+
+        ComboSelector.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        ComboSelector.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ComboSelectorActionPerformed(evt);
+            }
+        });
+
+        TextoDia.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TextoDiaActionPerformed(evt);
+            }
+        });
+
+        TextoConsulta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TextoConsultaActionPerformed(evt);
+            }
+        });
+
+        BotonAceptar.setText("Aceptar");
+        BotonAceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonAceptarActionPerformed(evt);
+            }
+        });
+
+        BotonCancelar.setText("Cancelar");
+        BotonCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonCancelarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(213, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(192, 192, 192))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(TextoInicial)
+                        .addGap(192, 192, 192))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(BotonCancelar)
+                        .addGap(49, 49, 49)
+                        .addComponent(BotonAceptar)
+                        .addGap(46, 46, 46))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(165, 165, 165)
+                        .addComponent(Texto1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(220, 220, 220)
+                        .addComponent(Texto2)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(271, 271, 271)
+                        .addComponent(ComboSelector, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(270, 270, 270)
+                        .addComponent(TextoDia, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(153, 153, 153)
+                        .addComponent(TextoMotivo))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(218, 218, 218)
+                        .addComponent(TextoConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 161, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(68, 68, 68)
-                .addComponent(jLabel1)
-                .addContainerGap(366, Short.MAX_VALUE))
+                .addComponent(TextoInicial)
+                .addGap(40, 40, 40)
+                .addComponent(Texto1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(ComboSelector, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(Texto2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(TextoDia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(TextoMotivo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(TextoConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(BotonAceptar)
+                    .addComponent(BotonCancelar))
+                .addGap(44, 44, 44))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -63,10 +160,38 @@ public class ModificarTurno extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void TextoDiaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextoDiaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TextoDiaActionPerformed
+
+    private void TextoConsultaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextoConsultaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TextoConsultaActionPerformed
+
+    private void BotonCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonCancelarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonCancelarActionPerformed
+
+    private void BotonAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonAceptarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonAceptarActionPerformed
+
+    private void ComboSelectorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ComboSelectorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ComboSelectorActionPerformed
+
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton BotonAceptar;
+    private javax.swing.JButton BotonCancelar;
+    private javax.swing.JComboBox<String> ComboSelector;
+    private javax.swing.JLabel Texto1;
+    private javax.swing.JLabel Texto2;
+    private javax.swing.JTextField TextoConsulta;
+    private javax.swing.JTextField TextoDia;
+    private javax.swing.JLabel TextoInicial;
+    private javax.swing.JLabel TextoMotivo;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
